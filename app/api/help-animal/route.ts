63d202d49986f806df70d1801bceaafd3d893a5a
25hp8413 User <25hp8413@G000-CC0000.local>
2026-01-24 13:48:14 +0800

    help animal create

---
+++ app/api/help-animal/route.ts
@@ -0,0 +1,39 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST (req:Request) {
+    try{
+
+        const body = await req.json()
+        const {location, description, images,status, userId , condition} = body
+
+        if(!location || !images || !userId || !description || !condition || !status) {
+            return NextResponse.json({error : "Missing field"})
+        }
+
+        const isUser = await prisma.user.findUnique({
+            where :{
+                 id : userId
+            }
+        }) 
+
+        if(!isUser) {
+            return NextResponse.json({error:"User not found"}, {status : 404})
+        }
+
+        const createdHelpAnimal = await prisma.helpAnimal.create({
+            data :  {
+                location,
+                description,
+                images,
+                status,
+                condition,
+                userId
+            }
+        })
+
+        return NextResponse.json(createdHelpAnimal, {status : 200})
+    }catch(err) {
+       return NextResponse.json({error : err}, {status : 500})
+    }
+}
