48b722a45577a0d835a5a56c3f9e8c7ad7c9ee93
25hp6016 User <25hp6016@G401-CC0116.local>
2026-01-24 14:09:36 +0800

    anket adopt animal

---
+++ _components/headbar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function HeadBar() {
+  return (
+    <div>
+    <div
+      style={{
+        width: "100%",
+        height: "4px",
+        background:
+          "linear-gradient(to right, red, yellow, green, blue)",
+      }}
+    />
+    </div>
+  );
+}
---
+++ app/api/anket/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { age, firstName, lastName, phoneNumber, secondaryPhone, location, district, address, email, hasPet, petId, userId , petInfo } = body;
+
+    if (!age || !firstName || !lastName || !phoneNumber || !location || !district || !email || hasPet === undefined || !petId || !userId) {
+      return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+    }
+
+    const isUser = await prisma.user.findUnique({ where: { id: userId } });
+    if (!isUser) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const adoptionForm = await prisma.adoptionForm.create({
+      data: {
+        age,
+        firstName,
+        lastName,
+        phoneNumber,
+        secondaryPhone,
+        location,
+        district,
+        address,
+        email,
+        hasPet,
+        petInfo,
+        petId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(adoptionForm, { status: 200 });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
