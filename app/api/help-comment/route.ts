8eb425501fce6fec1293c9c43707a9c24588dc2e
Sarangerel624 <sarangerelbatbold11@gmail.com>
2026-02-11 00:04:39 +0800

    a

---
+++ app/api/help-comment/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { userId, helpPostId, content, parentCommentId } = body;
+
+    if (!userId || !helpPostId || !content) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
+    }
+
+    const comment = await prisma.comment.create({
+      data: {
+        helpPostId,
+        userId,
+        content,
+        parentCommentId,
+      },
+    });
+
+    return NextResponse.json(comment, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+}
